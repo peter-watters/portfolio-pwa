7ec56a1f5ae94806ccf0d9f8e436cc93824fec71
Peter Watters <peter.j.watters@gmail.com>
2019-09-13 14:07:29 +0100

    Introduce constants and pages

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export default {
+  ROUTES: {
+    HOME: '/',
+    ABOUT: '/about',
+    BLOG: '/blog',
+    PROJECTS: '/projects',
+  },
+  SOCIAL:{
+    GITHUB: 'https://github.com/peter-watters',
+    LINKEDIN: 'https://www.linkedin.com/in/peter-watters/',
+    STACKOVERFLOW: 'https://stackoverflow.com/users/1365580/peadar',
+    TWITTER: 'https://twitter.com/Peter_J_W'
+  }
+}
