32ee3524d899b5065a942fbf277eac304bef644f
Peter Watters <peter.j.watters@gmail.com>
2019-09-30 19:21:02 +0100

    Setup firebase cloud function to proxy medium

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
 const functions = require('firebase-functions');
 var request = require('request');
-const myUser = '@peter.j.watters';
+// const myUser = '@peter.j.watters';
 
 exports.medium = functions.https.onRequest((req, res) => {
   if(!req.query.username) {
     return res.status(400).send('Error: You need to include query param ?username=@yourUsername');
   }  
-    const url = `https://medium.com/${req.query.username}/latest?format=json`;  
+    const url = `https://medium.com/@peter.j.watters/latest?format=json`;  
     return request(url,(error, response, body) => {
     const prefix = `])}while(1);</x>`
     const strip = payload => payload.replace(prefix, ``)
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,28 @@
 import React, { PureComponent } from 'react';
 import axios from 'axios';
+import Icon from '../components/Icon';
 import Page from '../components/Page';
 
 class Blog extends PureComponent {
-
-  componentDidMount(){
-    axios
-      .get('https://medium.com/@peter.j.watters/?format=json', { crossdomain: true })
-      .then(({ data })=> {
-      	console.log(data);
-      })
-      .catch((err)=> {})
+  state = { posts: [] } 
+  componentDidMount() {
+    this.fetchPosts().then(this.setPosts);
   }
+  fetchPosts = () => axios.get(`https://us-central1-portfolio-8db27.cloudfunctions.net/medium`);
+  setPosts = response => {
+    this.setState({
+      posts: response
+    });
+  } 
   render(){
-    return(<Page> 
-      <p>Blog</p>
-  </Page>)
+    return(
+    <Page>
+          <a href="https://medium.com/@peter.j.watters" target="_blank" rel="noopener noreferrer">
+            View My Medium
+          </a>
+              <Icon icon="medium" href="https://medium.com/@peter.j.watters" />
+          <pre>{JSON.stringify(this.state.posts, null, 2)}</pre>
+      </Page>)
   }
 }
 
