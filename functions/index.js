64da9aacddaed98c066012a3f7d57b1f34857e17
Peter Watters <peter.j.watters@gmail.com>
2019-09-30 19:03:02 +0100

    Add boilerplate medium function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+var request = require('request');
+const myUser = '@peter.j.watters';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.medium = functions.https.onRequest((req, res) => {
+  if(!req.query.username) {
+    return res.status(400).send('Error: You need to include query param ?username=@yourUsername');
+  }  
+    const url = `https://medium.com/${req.query.username}/latest?format=json`;  
+    return request(url,(error, response, body) => {
+    const prefix = `])}while(1);</x>`
+    const strip = payload => payload.replace(prefix, ``)
+    res.send(JSON.parse(strip(body)));
+  });
+})
