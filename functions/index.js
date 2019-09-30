53f57b3491946df73835984da7381a7f0f7e4f19
Peter Watters <peter.j.watters@gmail.com>
2019-09-30 19:34:31 +0100

    Refactor to try resolve CORS issue

--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,14 @@ var request = require('request');
 
 // Add CORS to your index.js
 const cors = require('cors')({origin: true});
-// Put this line to your function
-// Automatically allow cross-origin requests
-cors(req, res, () => {});
 
 exports.medium = functions.https.onRequest((req, res) => {
   if(!req.query.username) {
     return res.status(400).send('Error: You need to include query param ?username=@yourUsername');
   }  
+  // Put this line to your function
+  // Automatically allow cross-origin requests
+  cors(req, res, () => {});
     const url = `https://medium.com/@peter.j.watters/latest?format=json`;  
     return request(url,(error, response, body) => {
     const prefix = `])}while(1);</x>`;
