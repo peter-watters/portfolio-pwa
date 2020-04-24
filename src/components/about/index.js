1729e8155df3a0138695204360521a4f2e1cb12b
Peter Watters <peter.j.watters@gmail.com>
2020-04-24 05:38:35 +0100

    Refactor structure

--- src/components/about/index.js
+++
@@ -1,6 +0,0 @@
-import Adventures from './Adventures';
-import Contact from './Contact';
-import Story from './Story';
-import Work from './Work';
-
-export { Adventures, Contact, Story, Work };
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import About from './about';
-import Blog from './blog';
-import Home from './home';
-
-export { About, Blog, Home };
